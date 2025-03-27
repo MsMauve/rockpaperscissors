f259efa6fccf3978d65fd4da7e63c604503fde58
MsMauve <zoetropesilver@gmail.com>
2025-03-27 13:33:05 -0600

    add human choice logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    choiceSeed = Math.floor(Math.random() * 3) + 1;
+    const choiceSeed = Math.floor(Math.random() * 3) + 1;
     if (choiceSeed === 1) {
         return "Rock";
     } else if (choiceSeed === 2) {
@@ -10,5 +10,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    prompt("Rock, Paper, or Scissors?");
+   const humanChoice =  prompt("Rock, Paper, or Scissors?").toLowerCase();
+   const normalHumanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
+   return normalHumanChoice;
 }
+console.log(getHumanChoice());
